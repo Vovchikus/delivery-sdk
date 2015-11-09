b8f20966f46e94ccc8e87feb1c9e4dfb5230b04f
vzmultiship <vzmultiship@yandex-team.ru>
2015-11-09 12:11:10 +0300

    functions to class and methods

--- app/components/open_api/open_api_map.py
+++ app/components/open_api/open_api_map.py
@@ -2,8 +2,6 @@ from app.components import file_helper
 
 
 class OpenApi:
-    reformatted_data = dict()
-
     DELIVERY_URL = 'https://delivery.yandex.ru/api/last/'
     METHOD_KEYS = 'method_keys'
     RESOURCE_SETTINGS = 'resource_settings'
@@ -13,16 +11,20 @@ class OpenApi:
 
     @staticmethod
     def _reformat_request_data(request_data):
+        """
+        Weird hack to make PHP $_POST-like request with multidimensional arrays
+        (e.g. order_item[0 => [orderitem['name'] => 'name']])
+        :param request_data: dict
+        :return: dict
+        """
         result = dict()
         for k, v in sorted(request_data.iteritems()):
             if isinstance(request_data[k], list):
-                if k == 'order_items':
-                    i = 0
-                    for list_val in v:
-                        for sk, sv in sorted(list_val.iteritems()):
-                            result['order_items[' + str(i) + '][' + sk + ']'] = sv
-                    i += 1
-
+                i = 0
+                for list_val in v:
+                    for sk, sv in sorted(list_val.iteritems()):
+                        result['' + k + '[' + str(i) + '][' + sk + ']'] = sv
+                i += 1
             else:
                 if isinstance(request_data[k], bool):
                     request_data[k] = str(int(request_data[k]))
@@ -31,6 +33,12 @@ class OpenApi:
 
     @staticmethod
     def _generate_secret_key(method_key, data):
+        """
+        Secret key generator (prevent "man-in-the-middle" vulnerability)
+        :param method_key: string
+        :param data: dict
+        :return: string
+        """
         import hashlib
 
         result = []
@@ -43,10 +51,7 @@ class OpenApi:
             else:
                 if isinstance(val, bool):
                     val = str(int(val))
-                if isinstance(val, int):
-                    val = str(val)
                 result.append(val.encode('utf-8'))
-
         result.append(method_key.encode('utf-8'))
         data_listed = list(result)
         data_stringify = ''.join(data_listed)
@@ -55,6 +60,11 @@ class OpenApi:
         return m.hexdigest()
 
     def _sign_request_data(self, reformatted, method):
+        """
+        :param reformatted: dict
+        :param method: string
+        :return: dict
+        """
         method_keys = file_helper.get_json_from_file(self.METHOD_KEYS)
         resource_settings = file_helper.get_json_from_file(self.RESOURCE_SETTINGS)
         config_data = {
@@ -63,89 +73,39 @@ class OpenApi:
         }
         signed_data = reformatted.copy()
         signed_data.update(config_data)
-        secret_key = self._generate_secret_key(method_keys[method], reformatted)
+        secret_key = self._generate_secret_key(method_keys[method], signed_data)
         signed_data['secret_key'] = secret_key
         return signed_data
 
     def _init_request(self, method):
+        """
+        :param method: string
+        :return: :class:`Response <Response>` object
+        """
+        import requests
         data = self._sign_request_data(self.reformatted_data, method)
         return requests.post(self.DELIVERY_URL + method, data, verify=False)
 
     def create_order(self):
         """
-        :return: Response
+        :return: :class:`Response <Response>` object
         """
         return self._init_request('createOrder')
 
+    def autocomplete(self):
+        """
+        :return: :class:`Response <Response>` object
+        """
+        return self._init_request('autocomplete')
 
-import requests
-import open_api_helper
+    def search_delivery_list(self):
+        """
+        :return: :class:`Response <Response>` object
+        """
+        return self._init_request('searchDeliveryList')
 
-CREATE_ORDER_PATH = 'create_order.json'
-SEARCH_DELIVERY_LIST_PATH = 'search_delivery_list.json'
-
-
-def init_request(data, method):
-    """
-    :rtype object
-    :param data : dict
-    :param method: str
-    :return: Response | None
-    """
-    result = None
-    new_data = dict()
-
-    for k, v in sorted(data.iteritems()):
-        if isinstance(data[k], list):
-            if k == 'order_items':
-                i = 0
-                for list_val in v:
-                    for sk, sv in sorted(list_val.iteritems()):
-                        new_data['order_items[' + str(i) + '][' + sk + ']'] = sv
-                i += 1
-
-        else:
-            if isinstance(data[k], bool):
-                data[k] = str(int(data[k]))
-
-            new_data[k] = data[k]
-
-    try:
-        data_request = open_api_helper.sign_request_data(new_data, method)
-        result = requests.post(open_api_helper.DELIVERY_URL + method, data_request, verify=False)
-    except requests.exceptions.RequestException as ex:
-        print ex
-
-    return result
-
-
-def create_order(data):
-    """
-    :param data: dict
-    :return: Response
-    """
-    return init_request(data, 'createOrder')
-
-
-def search_delivery_list(data):
-    """
-    :param data: dict
-    :return: Response
-    """
-    return init_request(data, 'searchDeliveryList')
-
-
-def autocomplete(data):
-    """
-    :param data: dict
-    :return: Response
-    """
-    return init_request(data, 'autocomplete')
-
-
-def get_index(data):
-    """
-    :param data: dict
-    :return: Response
-    """
-    return init_request(data, 'getIndex')
+    def get_index(self):
+        """
+        :return: :class:`Response <Response>` object
+        """
+        return self._init_request('getIndex')
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, flash, request
 from app import app
 from forms import InitializationForm, OrderForm
 from app.components import file_helper
-from app.components.open_api import open_api_map, open_api_helper
+from app.components.open_api import open_api_map
 
 
 @app.route('/')
@@ -11,16 +11,16 @@ def index():
     """
     :return: `Template` object
     """
-    resource_settings = file_helper.get_json_from_file(open_api_helper.RESOURCE_SETTINGS)
-    method_keys = file_helper.get_json_from_file(open_api_helper.METHOD_KEYS)
+    resource_settings = file_helper.get_json_from_file(open_api_map.OpenApi.RESOURCE_SETTINGS)
+    method_keys = file_helper.get_json_from_file(open_api_map.OpenApi.METHOD_KEYS)
     return render_template("main.html", resource_settings=resource_settings, method_keys=method_keys)
 
 
 @app.route('/init', methods=['GET', 'POST'])
 def init():
     form = InitializationForm()
-    form.resource_settings.data = file_helper.get_json_from_file(open_api_helper.RESOURCE_SETTINGS)
-    form.method_keys.data = file_helper.get_json_from_file(open_api_helper.METHOD_KEYS)
+    form.resource_settings.data = file_helper.get_json_from_file(open_api_map.OpenApi.RESOURCE_SETTINGS)
+    form.method_keys.data = file_helper.get_json_from_file(open_api_map.OpenApi.METHOD_KEYS)
     if form.validate_on_submit():
         file_helper.make_file(form.method_keys.data, 'method_keys', 'json', 'w+')
         file_helper.make_file(form.resource_settings.data, 'resource_settings', 'json', 'w+')
@@ -32,7 +32,7 @@ def init():
 @app.route('/createOrder', methods=['GET', 'POST'])
 def create_order():
     order = file_helper.get_json_from_file('create_order') if file_helper.get_json_from_file('create_order') else ''
-    settings = file_helper.get_json_from_file(open_api_helper.RESOURCE_SETTINGS)
+    settings = file_helper.get_json_from_file(open_api_map.OpenApi.RESOURCE_SETTINGS)
     form = OrderForm()
     form.order_requisite.choices = [(r, r) for r in settings['requisite_ids']]
     form.order_warehouse.choices = [(w, w) for w in settings['warehouse_ids']]
@@ -48,27 +48,23 @@ def create_order():
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
-    call_api = open_api_map.autocomplete(request.args)
-    if call_api.status_code == 200:
-        return call_api.text
+    api = open_api_map.OpenApi(request.args)
+    autocomplete_api = api.autocomplete()
+    if autocomplete_api.status_code == 200:
+        return autocomplete_api.text
 
 
 @app.route('/searchDeliveryList', methods=['GET'])
 def search_delivery_list():
-    call_api = open_api_map.search_delivery_list(request.args)
-    if call_api.status_code == 200:
-        return call_api.text
+    api = open_api_map.OpenApi(request.args)
+    search_delivery_list_api = api.search_delivery_list()
+    if search_delivery_list_api.status_code == 200:
+        return search_delivery_list_api.text
 
 
 @app.route('/getIndex', methods=['GET'])
 def get_index():
-    call_api = open_api_map.get_index(request.args)
-    if call_api.status_code == 200:
-        return call_api.text
-
-
-@app.route('/createOrderJson', methods=['GET', 'POST'])
-def create_order_json():
-    call_api = open_api_map.create_order(request.args)
-    if call_api.status_code == 200:
-        return call_api.text
+    api = open_api_map.OpenApi(request.args)
+    get_index_api = api.search_delivery_list()
+    if get_index_api.status_code == 200:
+        return get_index_api.text
